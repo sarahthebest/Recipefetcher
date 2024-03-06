76cbb83f15292cda7c979c6a2d7e348eb1d49f67
Sarah <sarah.emmoth@proton.me>
2024-03-06 13:16:43 +0100

    edit color styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,25 @@ import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import Navbar from './Navbar.jsx';
-import { pink, blue } from '@mui/material/colors';
+import { purple, grey} from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: pink[500],
+      main: purple[900],
+      dark: grey[200]
     },
     secondary: {
-      main: blue[200],
+      main: purple[100],
     }
   },
+  spacing: 6,
 });
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Navbar />
       <App />
     </ThemeProvider>
   </React.StrictMode>
