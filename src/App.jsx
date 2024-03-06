7f503a9025ccaf99e8be156915a73c9d52c4e58c
sarah <sarah.emmoth@proton.me>
2024-03-06 10:26:03 +0100

    card edit

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,9 @@ function App() {
           alt="green iguana"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          {<Typography gutterBottom variant="h5" component="div">
             Lizard
-          </Typography>
+          </Typography>}
           <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
